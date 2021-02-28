dc42eeb8e39da961531d73dfdc474daef4b07377
Dasha <thisisdasha@gmail.com>
2021-02-28 12:05:18 +0300

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<template>
+    <button  @click="$emit('toggle-add-btn')" class="btn" :style="{background: color}">
+        {{ text }}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button', 
+    props: {
+        color: String,
+        text: String
+    }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+      <h1>{{ title }}</h1>
+      <Button  @toggle-add-btn="$emit('toggle-add-btn')" 
+      :text="showAddTask ? 'Close' : 'Add Task' " 
+      :color="showAddTask ? 'red' : 'green' "  />  
+  </header>
+
+</template>
+
+<script>
+
+import Button from './Button'
+
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+        showAddTask: Boolean,
+        color: String
+    },
+    components: {
+        Button
+    },
+    emits: ['toggle-add-btn']
+
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
